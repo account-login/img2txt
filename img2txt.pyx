e2d834f3f272d85dabc35fae7628e14b3c8a1319
root <account-login@users.noreply.github.com>
2021-10-05 17:12:16 +0800

    [WIP] add make_lines()

--- img2txt.pyx
+++ img2txt.pyx
@@ -3,11 +3,17 @@ from cython.operator cimport dereference
 from libc.stdint cimport *
 from libc.string cimport memcmp, memcpy
 from libcpp cimport bool
+from libcpp.algorithm cimport sort as std_sort
 from libcpp.string cimport string
 from libcpp.vector cimport vector
 from libcpp.unordered_set cimport unordered_set
 from libcpp.unordered_map cimport unordered_map
 from posix.unistd cimport read as posix_read
+
+
+cdef extern from *:
+    enum: PyUnicode_4BYTE_KIND
+    object PyUnicode_FromKindAndData(int kind, const void *buffer, Py_ssize_t size)
 
 
 include "htab.pyd"
@@ -62,21 +68,38 @@ cdef uint64_t space_hash(uint32_t w, uint32_t h, uint32_t const1, uint32_t const
 
 
 cdef struct CharPos:
-    uint32_t x, y
+    uint32_t x, y, w
     uint32_t code
     void *font
 
 
 cdef class CharResult:
-    cdef uint64_t stats_time_vhash_us
-    cdef uint64_t stats_time_loop_us
-    cdef uint64_t stats_lookup
-    cdef uint64_t stats_node
-    cdef uint64_t stats_check
-    cdef uint64_t stats_hit
-    cdef uint64_t stats_pos_total
+    cdef public uint64_t stats_time_vhash_us
+    cdef public uint64_t stats_time_loop_us
+    cdef public uint64_t stats_lookup
+    cdef public uint64_t stats_node
+    cdef public uint64_t stats_check
+    cdef public uint64_t stats_hit
+    cdef public uint64_t stats_pos_total
 
     cdef vector[CharPos] chars
+
+    def num_chars(self):
+        return self.chars.size()
+
+    def get_char(self, i):
+        if not (0 <= i < self.chars.size()):
+            raise KeyError(i)
+        return (self.chars[i].x, self.chars[i].y, self.chars[i].code)
+
+
+cdef struct CharGroup:
+    uint32_t w, h
+    vector[CharPos] chars
+
+
+cdef class LineResult:
+    cdef vector[CharGroup] lines
 
 
 cdef cppclass _CPPFont:
@@ -250,7 +273,7 @@ cdef CharResult run(_CPPFont *self, uint32_t *pixels, uint32_t w, uint32_t h):
                         continue
 
                     out.stats_hit += 1
-                    out.chars.push_back(CharPos(x=x, y=y, code=code, font=<void *>self))
+                    out.chars.push_back(CharPos(x=x, y=y, w=fw, code=code, font=<void *>self))
 
                 if x + fw < w:
                     hval <<= 1
@@ -324,6 +347,80 @@ cdef bool is_match(
     return True
 
 
+cdef bool less_by_pos(const CharPos &lhs, const CharPos &rhs):
+    cdef uint64_t k1 = ((<uint64_t>lhs.y) << 32) | lhs.x
+    cdef uint64_t k2 = ((<uint64_t>rhs.y) << 32) | rhs.x
+    if k1 == k2:
+        return lhs.w > rhs.w    # prioritize longer chars
+    else:
+        return k1 < k2
+
+
+cdef LineResult make_lines(_CPPFont *self, uint32_t *pixels, uint32_t w, uint32_t h, CharResult cr):
+    cdef LineResult out = LineResult()
+
+    std_sort(cr.chars.begin(), cr.chars.end(), &less_by_pos)
+
+    cdef CharGroup *line
+    cdef vector[uint8_t] used
+    cdef size_t i = 0, j, cur, used_cnt
+    cdef uint32_t x = 0
+    while i < cr.chars.size():
+        # group by y
+        j = i + 1
+        while j < cr.chars.size() and cr.chars[i].y == cr.chars[j].y:
+            j += 1
+
+        used_cnt = 0
+        used.clear()
+        used.resize(j - i)
+        while used_cnt < j - i:     # possibly overlapping lines
+            out.lines.push_back(CharGroup(w=0, h=self.font_h))
+            line = &out.lines.back()
+            for cur in range(i, j):
+                if used[cur - i]:
+                    continue
+                if line.chars.empty():
+                    x = cr.chars[cur].x
+                if x == cr.chars[cur].x:
+                    x += cr.chars[cur].w
+                    used[cur - i] = 1
+                    used_cnt += 1
+                    line.w += cr.chars[cur].w
+                    line.chars.push_back(cr.chars[cur])
+                elif x < cr.chars[cur].x:
+                    break
+                # else overlapping
+
+        i = j
+
+    # TODO: add spaces
+
+    # TODO: remove overlapping lines
+    cdef vector[CharGroup] buf
+    cdef vector[uint8_t] killed
+    i = 0
+    while i < out.lines.size():
+        x = out.lines[i].chars[0].x
+        y = out.lines[i].chars[0].y
+        j = i + 1
+        killed.clear()
+        killed.resize(1)
+        while j < out.lines.size() and out.lines[j].chars[0].y == y and out.lines[j].chars[0].x < x + out.lines[i].w:
+            killed.resize(j - i + 1)
+            if out.lines[j].chars[0].x + out.lines[j].w <= x + out.lines[i].w:
+                killed[j - i] = 1
+            j += 1
+        for cur in range(i, j):
+            if not killed[cur - i]:
+                buf.push_back(CharGroup(w=out.lines[cur].w, h=self.font_h))
+                buf.back().chars.swap(out.lines[cur].chars)
+        i = j
+    out.lines.swap(buf)
+
+    return out
+
+
 cdef class Font:
     cdef _CPPFont *wrapped
 
@@ -333,11 +430,14 @@ cdef class Font:
     def __dealloc__(self):
         del self.wrapped
 
-    cdef void load_bin_file(self, filename):
+    cpdef void load_bin_file(self, filename):
         load_bin_file(self.wrapped, filename)
 
-    cdef CharResult run(self, uint32_t *pixels, uint32_t w, uint32_t h):
-        return run(self.wrapped, pixels, w, h)
+    cpdef CharResult run(self, uintptr_t pixels, uint32_t w, uint32_t h):
+        return run(self.wrapped, <uint32_t *>pixels, w, h)
+
+    cpdef LineResult make_lines(self, uintptr_t pixels, uint32_t w, uint32_t h, CharResult cr):
+        return make_lines(self.wrapped, <uint32_t *>pixels, w, h, cr)
 
 
 cdef main():
@@ -377,7 +477,7 @@ cdef main():
     #         'load_factor', font.wrapped.mapping[i].load_factor()
     #     )
 
-    result = font.run(pixels.data(), w, h)
+    result = font.run(<uintptr_t>pixels.data(), w, h)
 
     print('stats_lookup', result.stats_lookup)
     print('stats_node', result.stats_node)
@@ -388,14 +488,20 @@ cdef main():
     print('stats_time_vhash_us', result.stats_time_vhash_us)
     print('stats_time_loop_us', result.stats_time_loop_us)
 
+    line_result = font.make_lines(<uintptr_t>pixels.data(), w, h, result)
+    cdef vector[uint32_t] pycodes
+    cdef size_t i
+    for i in range(line_result.lines.size()):
+        x = line_result.lines[i].chars[0].x
+        y = line_result.lines[i].chars[0].y
+        pycodes.clear()
+        for ch in line_result.lines[i].chars:
+            pycodes.push_back(ch.code)
+        py_str = PyUnicode_FromKindAndData(PyUnicode_4BYTE_KIND, pycodes.data(), pycodes.size())
+        print(y, x, x + line_result.lines[i].w, py_str)
+
     # for cp in result.chars:
     #     print(cp.x, cp.y, cp.code, chr(cp.code))
-
-
-# cdef struct CharPos:
-#     uint32_t x, y
-#     uint32_t code
-#     void *font
 
 
 if __name__ == '__main__':
